fd7b821a7bbd63047992753f7787e1bb9013fe04
Luiz Togni <contato.togni@gmail.com>
2025-08-15 10:43:35 -0300

    fix: init HttpResponse in http_response

--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, jsonify
+from src.types.http import HttpRequest
 from src.main.composer.create_user_composer import create_user_composer
 
 users_routes_bp = Blueprint("users_routes_bp", __name__)
 
 @users_routes_bp.route("/users", methods=["POST"])
 def create_user():
-    http_request = request.json
+    http_request = HttpRequest(body=request.json)
     view = create_user_composer()
     http_response = view.handle(http_request)
 
