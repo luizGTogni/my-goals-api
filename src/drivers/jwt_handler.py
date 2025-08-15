036f816adc3091b7855e25b7df929fdc740ea17c
Luiz Togni <contato.togni@gmail.com>
2025-08-14 22:52:29 -0300

    feat(drivers): add jwt handler

---
+++ src/drivers/jwt_handler.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from src.configs.jwt_configs import jwt_infos
+
+class JwtHandler:
+    def generate_token(self, body: dict = None) -> str:
+        token = jwt.encode(
+            payload={
+                "exp": datetime.now(timezone.utc) + timedelta(minutes=15),
+                **body
+            },
+            key=jwt_infos.secret_key,
+            algorithm=jwt_infos.algorithm
+        )
+
+        return token
+
+    def check_token(self, token: str) -> dict:
+        token_info = jwt.decode(
+            token,
+            key=jwt_infos.secret_key,
+            algorithms=jwt_infos.algorithm
+        )
+
+        return token_info
