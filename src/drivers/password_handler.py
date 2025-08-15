c9e4cb6065e980e92f0f057f6695e12a514da786
Luiz Togni <contato.togni@gmail.com>
2025-08-15 10:41:31 -0300

    refactor: add decode in generate token return

--- src/drivers/password_handler.py
+++ src/drivers/password_handler.py
@@ -3,7 +3,7 @@ import bcrypt
 class PasswordHandler:
     def encrypt_password(self, password_plain: str) -> str:
         hashed = bcrypt.hashpw(password_plain.encode("utf-8"), salt=bcrypt.gensalt(rounds=12))
-        return hashed
+        return hashed.decode("utf-8")
 
     def check_password(self, password_plain: str, password_hashed: str) -> bool:
-        return bcrypt.checkpw(password_plain.encode("utf-8"), password_hashed)
+        return bcrypt.checkpw(password_plain.encode("utf-8"), password_hashed.encode("utf-8"))
