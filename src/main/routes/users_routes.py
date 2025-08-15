884791fbd474bf099e3096ccdd94d96e90f26864
Luiz Togni <contato.togni@gmail.com>
2025-08-15 14:38:58 -0300

    feat: add login route

--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from src.main.composer.create_user_composer import create_user_composer
+from src.main.composer.login_composer import login_composer
 from src.middlewares.decorators.error_handler import error_handler
 from src.adapters import FlaskAdapter
 
@@ -9,5 +10,12 @@ users_routes_bp = Blueprint("users_routes_bp", __name__)
 @error_handler
 def create_user():
     view = create_user_composer()
-    flask_adapter = FlaskAdapter(view)
-    return flask_adapter.route_handler()
+    adapter = FlaskAdapter(view)
+    return adapter.route_handler()
+
+@users_routes_bp.route("/session", methods=["POST"])
+@error_handler
+def login():
+    view = login_composer()
+    adapter = FlaskAdapter(view)
+    return adapter.route_handler()
