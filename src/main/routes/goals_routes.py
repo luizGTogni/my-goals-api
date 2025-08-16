75d0b64008c7fcab0c3b64e4aa8af8a5e4c225ff
Luiz Togni <contato.togni@gmail.com>
2025-08-16 17:26:59 -0300

    feat: add create goal route

---
+++ src/main/routes/goals_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from src.main.composer.create_goal_composer import create_goal_composer
+from src.middlewares.decorators import protected_route
+from src.adapters import FlaskAdapter
+
+goals_routes_bp = Blueprint("goals_routes_bp", __name__)
+
+@goals_routes_bp.route("/goals", methods=["POST"])
+@protected_route
+def create_user(token_info: dict):
+    view = create_goal_composer()
+    adapter = FlaskAdapter(view)
+    return adapter.route_handler(token_info=token_info)
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
-from src.main.routes.users_routes import users_routes_bp
+from src.main.routes import users_routes_bp, goals_routes_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(users_routes_bp)
+app.register_blueprint(goals_routes_bp)
