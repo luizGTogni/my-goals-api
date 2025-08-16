0103d9ea670047b0b957733fb0de9e1b0a2a94d2
Luiz Togni <contato.togni@gmail.com>
2025-08-16 17:22:37 -0300

    feat: add token_info in parameters from route_handler

--- src/adapters/flask_adapter.py
+++ src/adapters/flask_adapter.py
@@ -6,12 +6,13 @@ class FlaskAdapter:
     def __init__(self, view: IView) -> None:
         self.__view = view
 
-    def route_handler(self, params: dict = None):
+    def route_handler(self, params: dict = None, token_info: dict = None):
         http_request = HttpRequest(
             body=request.json,
             params=params,
             query=request.args,
             headers=request.headers,
+            token_info=token_info,
         )
         http_response = self.__view.handle(http_request)
         return jsonify(http_response.body), http_response.status_code
