dfc45a83835b5b0d4ec653c87809b40a753184bf
Luiz Togni <contato.togni@gmail.com>
2025-08-16 22:39:13 -0300

    feat: add create task composer

---
+++ src/main/composer/create_task_composer.py
@@ -0,0 +1,16 @@
+from src.models.settings.connection import db_connection_handler
+from src.models.repositories.goals_repository import GoalsRepository
+from src.models.repositories.tasks_repository import TasksRepository
+from src.controllers.task.create_controller import CreateTaskController
+from src.views.task.create_view import CreateTaskView
+from src.views.interfaces.view import IView
+
+def create_task_composer() -> IView:
+    goals_repository = GoalsRepository(db_conn=db_connection_handler)
+    tasks_repository = TasksRepository(db_conn=db_connection_handler)
+    controller = CreateTaskController(
+        goals_repository=goals_repository,
+        tasks_repository=tasks_repository,
+    )
+    view = CreateTaskView(create_controller=controller)
+    return view
