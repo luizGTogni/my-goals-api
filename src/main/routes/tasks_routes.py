ade3fc97f8549c4ae9d023e74ad057c31c9b8405
Luiz Togni <contato.togni@gmail.com>
2025-08-17 19:45:27 -0300

    feat: add list all tasks route

--- src/main/routes/tasks_routes.py
+++ src/main/routes/tasks_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from src.main.composer.create_task_composer import create_task_composer
+from src.main.composer.list_all_tasks_composer import list_all_tasks_composer
 from src.middlewares.decorators import protected_route
 from src.adapters import FlaskAdapter
 
@@ -9,6 +10,16 @@ tasks_routes_bp = Blueprint("tasks_routes_bp", __name__)
 @protected_route
 def create_task(goal_id: str, token_info: dict):
     view = create_task_composer()
+    adapter = FlaskAdapter(view)
+    return adapter.route_handler(
+        params={ "goal_id": goal_id },
+        token_info=token_info
+    )
+
+@tasks_routes_bp.route("/goals/<goal_id>/tasks", methods=["GET"])
+@protected_route
+def list_all_tasks(goal_id: str, token_info: dict):
+    view = list_all_tasks_composer()
     adapter = FlaskAdapter(view)
     return adapter.route_handler(
         params={ "goal_id": goal_id },
