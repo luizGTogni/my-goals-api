0e59a9669359162e868acde2b81082f93bb589e3
Luiz Togni <contato.togni@gmail.com>
2025-08-17 16:31:46 -0300

    test: testing list all view

---
+++ src/views/goal/list_all_view_test.py
@@ -0,0 +1,23 @@
+from uuid import uuid4
+from src.types.http import HttpRequest, HttpResponse
+from .list_all_view import ListAllView
+
+USER_ID = str(uuid4())
+
+class ListAllControllerMock:
+    def __init__(self) -> None:
+        self.list_all_attributes = {}
+
+    def list_all(self, user_id: str, filters: dict = None) -> dict:
+        self.list_all_attributes["user_id"] = user_id
+        self.list_all_attributes["filters"] = filters
+
+def test_list_all_task():
+    mock_controller = ListAllControllerMock()
+    view = ListAllView(list_all_controller=mock_controller)
+    http_request = HttpRequest(token_info={ "user_id": USER_ID })
+    http_response = view.handle(http_request)
+
+    assert mock_controller.list_all_attributes["user_id"] == USER_ID
+    assert mock_controller.list_all_attributes["filters"] is None
+    assert isinstance(http_response, HttpResponse)
