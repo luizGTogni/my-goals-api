ddcbf97ffb085f152913dc3af3aa5cb904344406
Luiz Togni <contato.togni@gmail.com>
2025-08-21 20:30:25 -0300

    feat: add redis in composer create user

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from src.models.settings.sqlite_connection import sqlite_connection_handler
+from src.models.settings.redis_connection import redis_connection_handler
 from src.main.server import app
 
 if __name__ == "__main__":
     sqlite_connection_handler.connect()
+    redis_connection_handler.connect()
     app.run(host="0.0.0.0", port=3000, debug=True)
--- src/main/composer/create_user_composer.py
+++ src/main/composer/create_user_composer.py
@@ -1,11 +1,18 @@
 from src.models.settings.sqlite_connection import sqlite_connection_handler
+from src.models.settings.redis_connection import redis_connection_handler
 from src.models.repositories.users_repository import UsersRepository
+from src.models.repositories.redis_repository import RedisRepository
 from src.controllers.user.create_controller import CreateUserController
 from src.views.user.create_view import CreateUserView
 from src.views.interfaces.view import IView
 
 def create_user_composer() -> IView:
     repository = UsersRepository(db_conn=sqlite_connection_handler)
-    controller = CreateUserController(users_repository=repository)
+    conn = redis_connection_handler.get_connection()
+    redis_repository = RedisRepository(redis_conn=conn)
+    controller = CreateUserController(
+        users_repository=repository,
+        redis_repository=redis_repository
+    )
     view = CreateUserView(create_controller=controller)
     return view
